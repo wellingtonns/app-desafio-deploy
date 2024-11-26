6175bf9fde21bb4ea2c9d58e91f1e8ad1eba8b1e
wellingtonrptech <wellington.rptech@gmail.com>
2024-11-26 17:04:36 -0300

    Comit Deploy

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo " Criando Imagens ......."
+
+docker build -t welignton/projeto-back:1.0 backend/.
+docker build -t welignton/db-mysql:1.0 database/.
+
+echo " Subindo imagens...."
+
+docker push welignton/projeto-back:1.0
+docker push welignton/db-mysql:1.0
+
+echo "Criando Serviços no cluster Kubernetes..."
+
+kubectl apply -f ./services.yaml
+
+echo "Criando Deployments no cluster Kubernetes..."
+
+kubectl apply -f ./deployment.yaml
